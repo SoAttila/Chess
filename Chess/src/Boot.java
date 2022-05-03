85893b2e415cb42dd39356538d86c0edd9ed4258
Somogyi Attila <tpix3d@inf.elte.hu>
2022-05-03 23:00:29 +0200

    Updating

--- Chess/src/Boot.java
+++ Chess/src/Boot.java
@@ -2,8 +2,6 @@ import gui.ChessFrame;
 
 public class Boot {
     public static void main(String[] args) {
-        //Board board=new Board();
-        //System.out.println(board);
         java.awt.EventQueue.invokeLater(() -> {
             new ChessFrame();
         });
